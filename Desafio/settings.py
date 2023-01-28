419cce33b9036fa6c5643c19e5bf19a96eba295d
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2023-01-28 00:59:07 -0300

    estrutura criada

--- Desafio/settings.py
+++ Desafio/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'datas',
+    'storage',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Store(models.Model):
+    
+    name = models.CharField(max_length=19, unique=True)
+    owner = models.CharField(max_length=14)
